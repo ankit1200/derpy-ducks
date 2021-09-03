8cb86cba88b37a8e0f9536f5da04fd74cdcf5398
ankit1200 <ankit1200@gmail.com>
2021-09-02 22:53:51 -0400

    added more ducks, renamed files

--- js/ducks.js
+++ js/ducks.js
@@ -8,12 +8,17 @@ function startTimer() {
 }
 
 var images = [], x = -1;
-images[0] = "images/IMG_0681.PNG";
-images[1] = "images/IMG_0682.PNG";
-images[2] = "images/IMG_0683.PNG";
-images[3] = "images/IMG_0684.PNG";
-images[4] = "images/IMG_0685.PNG";
-images[5] = "images/IMG_0686.PNG";
-images[6] = "images/IMG_0687.PNG";
-images[7] = "images/IMG_0688.PNG";
-images[8] = "images/IMG_0689.PNG";
+images[0] = "images/Artboard1@3x.png";
+images[1] = "images/Artboard2@3x.png";
+images[2] = "images/Artboard3@3x.png";
+images[3] = "images/Artboard4@3x.png";
+images[4] = "images/Artboard5@3x.png";
+images[5] = "images/Artboard6@3x.png";
+images[6] = "images/Artboard7@3x.png";
+images[7] = "images/Artboard8@3x.png";
+images[8] = "images/Artboard9@3x.png";
+images[9] = "images/Artboard10@3x.png";
+images[10] = "images/Artboard11@3x.png";
+images[11] = "images/Artboard12@3x.png";
+images[12] = "images/Artboard13@3x.png";
+images[13] = "images/Artboard14@3x.png";
